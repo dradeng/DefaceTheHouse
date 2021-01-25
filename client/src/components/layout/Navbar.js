dc860f964a60df0a2bf0c741f79f605da8a84d3a
dradeng <dsg2dm@virginia.edu>
2021-01-25 15:40:51 -0800

    initial codebase

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,55 +1,117 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
 import { logout } from '../../actions/auth';
+import logo from '../../img/wreck.png';
 
 const Navbar = ({ auth: { isAuthenticated }, logout }) => {
-  const authLinks = (
-    <ul>
-      <li>
-        <Link to="/profiles">Developers</Link>
-      </li>
-      <li>
-        <Link to="/posts">Posts</Link>
-      </li>
-      <li>
-        <Link to="/dashboard">
-          <i className="fas fa-user" />{' '}
-          <span className="hide-sm">Dashboard</span>
-        </Link>
-      </li>
-      <li>
-        <a onClick={logout} href="#!">
-          <i className="fas fa-sign-out-alt" />{' '}
-          <span className="hide-sm">Logout</span>
-        </a>
-      </li>
-    </ul>
-  );
+  const sportsOptions = [
+    { value: 'NFL', label: 'NFL' },
+    { value: 'NCAAF', label: 'NCAAF' },
+    { value: 'NBA', label: 'NBA' },
+    { value: 'MLB', label: 'MLB' },
+    { value: 'NHL', label: 'NHL' },
+  ];
+  const picksOptions = [
+    { value: 'NFL', label: 'NFL' },
+    { value: 'NCAAF', label: 'NCAAF' },
+    { value: 'NBA', label: 'NBA' },
+    { value: 'MLB', label: 'MLB' },
+    { value: 'NHL', label: 'NHL' },
+  ]
+  const bettingOptions = [
+    { value: 'BettingSitesByRegion', label: 'Betting Sites By Region' },
+    { value: 'BettingSitesBySport', label: 'BettingSites By Sport' },
+    { value: 'OnlineSportsbook', label: 'Online Sportsbook' },
+  ];
+  const [formData, setFormData] = useState({
+    showPicks: false,
+    showBetting: false,
+    showSports: false
+  });
 
-  const guestLinks = (
-    <ul>
-      <li>
-        <Link to="/profiles">Developers</Link>
+  const {
+    showPicks,
+    showBetting,
+    showSports,
+  } = formData;
+
+
+  const changeSports = () => {
+    setFormData({
+        ...formData,
+        showSports: !showSports
+    });
+  }
+  const changePicks = () => {
+    setFormData({
+        ...formData,
+        showPicks: !showPicks
+    });
+  }
+  const changeBetting = () => {
+    setFormData({
+        ...formData,
+        showBetting: !showBetting
+    });
+  }
+  
+  const links = (
+    <ul style={{verticalAlign: 'middle'}}>
+      <li style={{ width: '100px'}}>
+        <div style={{height: '1px',}}>
+          <div onClick={() => changeSports()}>SPORTS</div>
+          <div style={{display: showSports ? 'inline-block' : 'none', visible: 'auto', fontWeight: '700', backgroundColor: '#F7F6F4', color: '#000000', borderRadius: '15px'}}>
+            <Link to="/nfl" style={{ margin: '20px', display: 'block', color: '#000000'}}>NFL</Link>
+            <Link to="/" style={{ margin: '20px', display: 'block', color: '#000000'}}>NCAAF</Link>
+            <Link to="/" style={{ margin: '20px', display: 'block', color: '#000000'}}>NBA</Link>
+            <Link to="/" style={{ margin: '20px', display: 'block', color: '#000000'}}>MLB</Link>
+            <Link to="/" style={{ margin: '20px', display: 'block', color: '#000000'}}>NHL</Link>
+          </div>
+        </div>
       </li>
-      <li>
-        <Link to="/register">Register</Link>
+      <li style={{ width: '100px'}}>
+        <div style={{height: '1px',}}>
+          <div onClick={() => changePicks()}>PICKS</div>
+          <div style={{display: showPicks ? 'inline-block' : 'none', visible: 'auto', fontWeight: '700', backgroundColor: '#F7F6F4', color: '#000000', borderRadius: '15px'}}>
+            <div style={{ margin: '20px'}}>NFL</div>
+            <div style={{ margin: '20px'}}>NCAAF</div>
+            <div style={{ margin: '20px'}}>NBA</div>
+            <div style={{ margin: '20px'}}>MLB</div>
+            <div style={{ margin: '20px'}}>NHL</div>
+          </div>
+        </div>
       </li>
-      <li>
-        <Link to="/login">Login</Link>
+      <li style={{ width: '100px'}}>
+        <div style={{height: '1px',}}>
+          <div onClick={() => changeBetting()}>BETTING</div>
+          <div style={{display: showBetting ? 'inline-block' : 'none', visible: 'auto', fontWeight: '700', backgroundColor: '#F7F6F4', color: '#000000', borderRadius: '15px'}}>
+            <div style={{ margin: '20px'}}>NFL</div>
+            <div style={{ margin: '20px'}}>NCAAF</div>
+            <div style={{ margin: '20px'}}>NBA</div>
+            <div style={{ margin: '20px'}}>MLB</div>
+            <div style={{ margin: '20px'}}>NHL</div>
+          </div>
+        </div>
       </li>
     </ul>
   );
 
   return (
-    <nav className="navbar bg-dark">
+    <nav className="navbar bg-dark" style={{height: '100px', backgroundColor: '#00BFFF'}}>
       <h1>
         <Link to="/">
-          <i className="fas fa-code" /> DevConnector
+          <img
+            style={{width: '30px', height: '30px',marginBottom: '-2px', marginRight: '10px'}}
+            src={logo}
+          />
+          DefaceTheHouse
         </Link>
       </h1>
-      <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      <Fragment>{links}</Fragment>
     </nav>
   );
 };
---
+++ client/src/components/picks/nfl/2020/KC_TB_2_7_2020.js
@@ -0,0 +1,67 @@
+import React, { Fragment } from 'react';
+
+const KC_TB_2_7_2020 = () => {
+  return (
+    <div style={{textAlign: 'center', margin: '60px 0px'}}>
+      <div style={{fontSize: '40px', fontWeight: '700', marginBottom: '40px'}}>
+        Kansas City Chiefs vs Tampa Bay Buccaneers
+      </div>
+	  	<div>
+	      <div style={{display: 'inline-block', width: '300px'}}>
+	      	<div style={{fontSize: '24px', fontWeight: '700'}}>Kansas City Chiefs</div>
+	      	<div style={{fontSize: '24px', fontWeight: '700'}}>9-1 (6-4) ATS</div>
+	      </div>
+	      <div style={{display: 'inline-block', width: '300px'}}>
+	      	<div>February 07, 2021, 6:30 PM ET</div>
+	      	<div>Raymond James Stadium</div>
+	      	<div>Predicted Score</div>
+	      	<div style={{marginTop: '40px'}}>
+	      		<div style={{display: 'inline-block', float: 'left', fontSize: '24px', fontWeight: '700'}} >28.5</div>
+	      		<div style={{display: 'inline-block', float: 'right', fontSize: '24px', fontWeight: '700'}} >20.9</div>
+	      	</div>
+	      </div>
+	      <div style={{display: 'inline-block', width: '300px'}}>
+	      	<div style={{fontSize: '24px', fontWeight: '700'}}>Tampa Bay Buccaneers</div>
+	      	<div style={{fontSize: '24px', fontWeight: '700'}}>7-4 (5-6) ATS</div>
+	      </div>
+	    </div>
+	    <div style={{marginTop: '40px'}}>
+	    	<div style={{display: 'inline-block', width: '300px'}}>
+	    	  <div>Best KC Spread</div>
+
+	    	  <div>Best KC Moneyline</div>
+	    	</div>
+	    	<div style={{display: 'inline-block', width: '300px'}}>
+	    		<div>Kansas City</div>
+	    		<div>will win, cover the spread, and the total will go under</div>
+	    		<div>
+	    			<div>Spread Consensus</div>
+	    			<div style={{display: 'inline-block', float: 'left'}}>
+	    				KC 59%: -3
+	    			</div>
+	    			<div style={{display: 'inline-block', float: 'right'}}>
+	    				TB 41%: +3
+	    			</div>
+	    		</div>
+	    		<div style={{marginTop: '40px'}}>
+	    			<div>Total Consensus 56.5</div>
+	    			<div style={{display: 'inline-block', float: 'left'}}>
+	    				UNDER: 64%
+	    			</div>
+	    			<div style={{display: 'inline-block', float: 'right'}}>
+	    				OVER: 36%
+	    			</div>
+	    		</div>
+	    		<div>Best Over/Under</div>
+	    	</div>
+	    	<div style={{display: 'inline-block', width: '300px'}}>
+	    	  <div>Best KC Spread</div>
+
+	    	  <div>Best KC Moneyline</div>
+	    	</div>
+	    </div>
+    </div>
+  );
+};
+
+export default KC_TB_2_7_2020;
