d1e866605fb48e233c697ecf39619d7c65325bd0
dradeng <dsg2dm@virginia.edu>
2021-01-27 10:53:50 -0800

    pie charts

--- client/src/components/picks/nfl/2020/KC_TB_2_7_2020.js
+++ client/src/components/picks/nfl/2020/KC_TB_2_7_2020.js
@@ -7,8 +7,12 @@ const KC_TB_2_7_2020 = () => {
 
 
 	const dataMock = [
-  	{ title: 'KC -3 55%', value: 59, color: '#FFB81C' },
-  	{ title: 'TB +3 45%', value: 41, color: '#D40909' },
+  	{ title: 'KC 55%', value: 59, color: '#FFB81C' },
+  	{ title: 'TB 45%', value: 41, color: '#D40909' },
+	];
+	const dataEspn = [
+  	{ title: 'KC 52.1%', value: 52.1, color: '#FFB81C' },
+  	{ title: 'TB 47.9%', value: 47.9, color: '#D40909' },
 	];
 	const dataMock2 = [
   	{ title: 'Under 59%', value: 59, color: '#FFB81C' },
@@ -29,7 +33,7 @@ const KC_TB_2_7_2020 = () => {
 	      <div style={{display: 'inline-block', width: '300px'}}>
 	      	<div>February 07, 2021, 6:30 PM ET</div>
 	      	<div>Raymond James Stadium</div>
-	      	<div>Predicted Score</div>
+	      	<div>Computer Model Predicted Score</div>
 	      	<div style={{marginTop: '40px'}}>
 	      		<div style={{display: 'inline-block', float: 'left', fontSize: '24px', fontWeight: '700'}} >28.5</div>
 	      		<div style={{display: 'inline-block', float: 'right', fontSize: '24px', fontWeight: '700'}} >20.9</div>
@@ -41,8 +45,8 @@ const KC_TB_2_7_2020 = () => {
 	      </div>
 	    </div>
 	    <div style={{display: 'inline-block'}}>
-	    	<div style={{width: '300px', margin: '0px 80px'}}>
-	  			<div style={{fontSize: '24px', fontWeight: '700', marginTop: '40px', marginBottom: '20px'}}>Spread Consensus</div>
+	    	<div style={{width: '280px', margin: '0px 40px'}}>
+	  			<div style={{fontSize: '24px', fontWeight: '700', marginBottom: '20px'}}>Spread Consensus 3.5</div>
 	  			<PieChart
 	  				style={{height: '200px'}}
 			      data={dataMock}
@@ -60,9 +64,29 @@ const KC_TB_2_7_2020 = () => {
 			    />
 	  		</div>
 	  	</div>
+	  	<div style={{display: 'inline-block'}}>
+	    	<div style={{width: '280px', margin: '0px 40px'}}>
+	  			<div style={{fontSize: '24px', fontWeight: '700', marginBottom: '20px'}}>ESPN Prediction</div>
+	  			<PieChart
+	  				style={{height: '200px'}}
+			      data={dataEspn}
+			      lineWidth={20}
+			      paddingAngle={18}
+			      rounded
+			      label={({ dataEntry }) => dataEntry.title}
+			      labelStyle={(index) => ({
+			        fill: dataEspn[index].color,
+			        fontSize: '7px',
+			        fontFamily: 'sans-serif',
+			        fontWeight: '700' 
+			      })}
+			      labelPosition={30}
+			    />
+	  		</div>
+	  	</div>
   		<div style={{marginTop: '40px', display: 'inline-block'}}>
-  		  <div style={{width: '300px', margin: '0px 80px'}}>
-	  			<div style={{fontSize: '24px', fontWeight: '700', marginTop: '40px', marginBottom: '20px'}}>Total Consensus 56.5</div>
+  		  <div style={{width: '280px', margin: '0px 40px'}}>
+	  			<div style={{fontSize: '24px', fontWeight: '700', marginBottom: '20px'}}>Total Consensus 56.5</div>
 	  			<PieChart
 	  				style={{height: '200px'}}
 			      data={dataMock2}
@@ -82,43 +106,63 @@ const KC_TB_2_7_2020 = () => {
   		</div>
 	    <div style={{marginTop: '40px'}}>
 	    	<div style={{display: 'inline-block', width: '300px'}}>
-	    	  <div style={{paddingTop: '20px', marginBottom: '20px'}}>Best KC Spread</div>
+	    	  <div style={{paddingTop: '20px', marginBottom: '20px', fontSize: '24px', fontWeight: '700'}}>Best KC Spread</div>
 	    	  <a href="https://link.intertops.eu/c/408817/" target="_blank" >
 
-	    	  	<img style={{width: '200px'}} src={intertops} />
+	    	  	<img style={{width: '200px', borderRadius: '8px'}} src={intertops} />
+	    	  	<div style={{marginTop: '6px', fontWeight: '700' , color: 'green'}}>
+	    	  		-105
+	    	  	</div>
 	    	  </a>
 
-	    	  <div style={{paddingTop: '20px', marginBottom: '20px'}}>Best KC Moneyline</div>
+	    	  <div style={{paddingTop: '20px', marginBottom: '20px', fontSize: '24px', fontWeight: '700'}}>Best KC Moneyline</div>
 	    	  <a href="https://link.intertops.eu/c/408817/" target="_blank" >
 
-	    	  	<img style={{width: '200px'}} src={intertops} />
+	    	  	<img style={{width: '200px', borderRadius: '8px'}} src={intertops} />
+	    	  	<div style={{marginTop: '6px', fontWeight: '700' , color: 'green'}}>
+	    	  		-150
+	    	  	</div>
 	    	  </a>
 	    	</div>
 	    	<div style={{display: 'inline-block'}}>
 	    		<div style={{marginTop: '80px'}}>
-	    		  <div style={{marginBottom: '20px'}}>Best Over</div>
+	    		  <div style={{marginBottom: '20px', fontSize: '24px', fontWeight: '700'}}>Best Over</div>
 	    			<a href="https://link.intertops.eu/c/408817">
-	    	  		<img style={{width: '200px'}} src={intertops} />
+	    	  		<img style={{width: '200px', borderRadius: '8px'}} src={intertops} />
+	    	  		<div style={{marginTop: '6px', fontWeight: '700' , color: 'green'}}>
+	    	  			-110
+	    	  		</div>
 	    	  	</a>
 	    	  </div>
 	    	  <div>
-	    	  	<div style={{paddingTop: '20px', marginBottom: '20px'}}style={{paddingTop: '20px', marginBottom: '20px'}}>Best Under</div>
+	    	  	<div style={{paddingTop: '20px', marginBottom: '20px', fontSize: '24px', fontWeight: '700'}}>Best Under</div>
 		    	  <a href="https://link.intertops.eu/c/408817">
-		    	  	<img style={{width: '200px'}} src={intertops} />
+		    	  	<img style={{width: '200px', borderRadius: '8px'}} src={intertops} />
+		    	  	<div style={{marginTop: '6px', fontWeight: '700' , color: 'green'}}>
+	    	  			-110
+	    	  		</div>
 		    	  </a>
 		    	 </div>
 
 	    	</div>
 	    	<div style={{display: 'inline-block', width: '300px'}}>
-	    	  <div style={{paddingBottom: '20px'}}>Best TB Spread</div>
+	    	  <div style={{paddingBottom: '20px', fontSize: '24px', fontWeight: '700'}}>Best TB Spread</div>
+	    	  
 	    	  <a href="https://link.intertops.eu/c/408817/" target="_blank" >
 
-	    	  	<img style={{width: '200px'}} src={intertops} />
+	    	  	<img style={{width: '200px', borderRadius: '8px'}} src={intertops} />
+	    	  	<div style={{marginTop: '6px', fontWeight: '700' , color: 'green'}}>
+	    	  		-115
+	    	  	</div>
 	    	  </a>
-	    	  <div style={{paddingTop: '20px', marginBottom: '20px'}}>Best TB Moneyline</div>
+	    	  <div style={{paddingTop: '20px', marginBottom: '20px', fontSize: '24px', fontWeight: '700'}}>Best TB Moneyline</div>
 	    	  <a href="https://link.intertops.eu/c/408817">
-	    	  	<img style={{width: '200px'}} src={intertops} />
+	    	  	<img style={{width: '200px', borderRadius: '8px'}} src={intertops} />
+	    	  	<div style={{marginTop: '6px', fontWeight: '700' , color: 'green'}}>
+	    	  		+170
+	    	  	</div>
 	    	  </a>
+	    	  
 	    	</div>
 	    </div>
     </div>
