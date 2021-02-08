39a04a4c5fe851ddd88f916ed14658ca3e43654d
dradeng <dsg2dm@virginia.edu>
2021-02-08 11:43:23 -0800

    retarded

--- client/src/components/picks/nba/2020/feb/Feb_8_21_WizardsBulls.js
+++ client/src/components/picks/nba/2020/feb/Feb_8_21_WizardsBulls.js
@@ -28,6 +28,7 @@ const Feb_8_21_WizardsBulls = () => {
         Washington Wizards vs Chicago Bulls
       </div>
 	  	<div>
+	  	
 	      <div class='leftScorePanel' style={{display: 'inline-block', verticalAlign: 'top'}}>
 	      	<div style={{fontWeight: '700'}}>  Washington Wizards</div>
 	      	<div style={{fontWeight: '700'}}>5-15 (8-12) ATS</div>
